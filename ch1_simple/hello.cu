c937729fa8352fad3ff9914c6df46f27ed4ecf8b
Canta Tronic <tokareva@jinr.ru>
2018-02-09 22:28:36 +0300

    Make a little changes

--- ch1_simple/hello.cu
+++ ch1_simple/hello.cu
@@ -1,14 +1,16 @@
 
-#include <cuda_runtime.h> 
-#include <iostream>
+#include <cuda_runtime.h>
+//#include <iostream>
+#include <cstdio>
 
-__global__ void test(char * name) {
-//   printf ("Hello, %s", name);
-  std::cout<<std::endl;
+__global__ void test(/* const char * name -- CUDA cannot handle an array directly without cudaMemCpy */) {
+  printf ("Hello\n");
+//  printf ("Hello, %s\n", name);
+//  std::cout<<std::endl; // CUDA cannot handle std::cout inside a kernel
 }
 
 int main(){
-  test<<<1,3>>>("Test");
+  test<<<1,3>>>(/* "Test" */);
   cudaDeviceSynchronize();
   return 0;
 }
--- ch1_simple/test_arithm.cu
+++ ch1_simple/test_arithm.cu
@@ -4,14 +4,14 @@
 
 using namespace std;
 
-__global__ void test_add() {
-//   a += b;
+__global__ void test_add(int a, int b) { // added parameters int a, int b
+   a += b;
 }
 
 int main() {
 //   cout<<(test_add<<<1,1>>>(4,5))<<endl;
   test_add<<<1,1>>>(4,5);
-  CudaDeviceSinchronize();
-  CudaDeviceReset();
+  cudaDeviceSynchronize(); // was CudaDeviceSinchronize
+  cudaDeviceReset(); // was CudaDeviceReset
   return 0;
-} 
+}
